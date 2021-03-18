c4774052c0bdd7830e00b300cce13bf6859f25cf
Florencia Singh <florencia.singh@gmail.com>
2021-03-17 21:08:11 -0300

    router and layout

--- src/page-search-result.js
+++ src/page-search-result.js
@@ -7,11 +7,24 @@ class PageSearchResult extends Component {
   state = {
     busqueda: "",
   };
+  componentDidMount() {
+    let search = this.props.history.location.search
+      .substr(1)
+      .replace("%20", " ");
+
+    this.setState({
+      busqueda: search,
+    });
+  }
+  componentWillMount() {}
+  componentWillUnmount() {}
+
   changeHandle = (e) => {
     this.setState({
       [e.target.name]: e.target.value,
     });
   };
+
   render() {
     return (
       <React.Fragment>
